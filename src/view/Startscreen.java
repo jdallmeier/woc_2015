1c00fcc9abaee2429f3b13053490b57ffe26c0a8
Jonas Dallmeier <dallmeie@fim.uni-passau.de>
2015-07-04 01:12:20 +0200

    startscreen

--- src/view/Startscreen.java
+++ src/view/Startscreen.java
@@ -3,12 +3,16 @@ package view;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Jonas on 03.07.2015.
  */
-public class Startscreen extends JFrame{
+public class Startscreen extends JFrame implements ActionListener{
     private JPanel ContentPane = null;
+    private JButton start;
+    private JButton end;
 
     public Startscreen() {
         super();
@@ -25,7 +29,7 @@ public class Startscreen extends JFrame{
         if(ContentPane == null){
             ContentPane = new JPanel();
             ContentPane.setLayout(new BorderLayout());
-            ContentPane.setBorder(new EmptyBorder(20,20,20,20));
+            ContentPane.setBorder(new EmptyBorder(120, 0, 0, 0));
 
             JPanel labelPanel = new JPanel();
             labelPanel.setLayout(new FlowLayout());
@@ -34,14 +38,26 @@ public class Startscreen extends JFrame{
 
             JPanel buttonPanel = new JPanel();
             buttonPanel.setLayout(new FlowLayout());
-            JButton start = new JButton("Spiel starten");
+            start = new JButton("Spiel starten");
+            start.addActionListener(this);
             buttonPanel.add(start);
-            JButton end = new JButton("Spiel verlassen");
+            end = new JButton("Spiel verlassen");
+            end.addActionListener(this);
             buttonPanel.add(end);
 
-            ContentPane.add(labelPanel, BorderLayout.NORTH);
+            ContentPane.add(labelPanel, BorderLayout.PAGE_START);
             ContentPane.add(buttonPanel, BorderLayout.CENTER);
         }
         return ContentPane;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == start){
+            JOptionPane.showMessageDialog(null, "test");
+        }else if(e.getSource() == end){
+            System.exit(0);
+        }
+
     }
 }
