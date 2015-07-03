94d6452afc575e7051332ed1d1358d38927186d6
Jonas <poehler@fim.uni-passau.de>
2015-07-04 00:06:28 +0200

    added Model tweaked Gui, Controller, Layer

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,7 @@
 package controller;
 
-import model.Layer;
-import model.Player;
+
+import model.Model;
 import view.Gui;
 
 /**
@@ -9,14 +9,10 @@ import view.Gui;
  */
 public class Controller {
     Gui ui;
-    Layer layer;
-    Player right_player;
-    Player left_player;
+    Model model;
     public Controller(Gui g){
         ui = g;
-        layer = new Layer(ui.getWidth(), ui.getHeigth());
-        right_player = new Player();
-        left_player = new Player();
+        model = new Model(ui.getWidth(), ui.getHeigth());
     }
 
 }
--- src/model/Model.java
+++ src/model/Model.java
@@ -4,4 +4,12 @@ package model;
  * Created by Jonas on 03.07.2015.
  */
 public class Model {
+    Layer layer;
+    Player right_player;
+    Player left_player;
+    public Model(int width, int height){
+        layer = new Layer(width, height);
+        right_player = new Player();
+        left_player = new Player();
+    }
 }
